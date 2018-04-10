589c20ad5e99651cc0cc3179c365d578f41e2f76
stehan253 <stehan253@E29652.edu.linkoping.se>
2018-04-10 10:58:43 +0200

    Hugo spelar för mycket Grepolis

--- src/musicregister/MusicRegister.java
+++ src/musicregister/MusicRegister.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package musicregister;
 
-/**
- *
- * @author stehan253
- */
+import java.util.ArrayList;
+
 public class MusicRegister {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
