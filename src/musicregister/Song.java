65ee32544bd21b7f2ae9089e5e0b5292cda31721
Hansson Stefan (Elev <stehan253@edu.linkoping.se>
2018-04-16 15:30:43 +0200

    added class for temporal storage

---
+++ src/musicregister/Song.java
@@ -0,0 +1,21 @@
+package musicregister;
+
+public class Song {
+
+    private int keyNumber;
+    private String adress;
+    private String owner;
+    
+    public Song() {
+        
+    }
+    
+    public Song(int keyNumber, String adress, String owner) {
+        
+        this.keyNumber = keyNumber;
+        this.adress = adress;
+        this.owner = owner;
+        
+    }
+    
+}
